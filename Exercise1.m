99262fc39c8857f6be76c7d26ddc55ff0a82ab83
Daniel Landberg <45692458+daniellandberg@users.noreply.github.com>
2020-09-02 14:24:26 +0200

    Add files via upload

--- Exercise1.m
+++ Exercise1.m
@@ -1,19 +1,122 @@
 clc
 clear all
-
+legend('10','20','40', '80', '160', '320')
 N = [10,20,40,80,160,320]; 
 T = cell(1,length(N));
 U = cell(1,length(N));
-f=@(t, y) [y(2); (1-y(1)^2)*y(2)-y(1)];
 
+f=@(y) [y(2); (1-y(1)^2)*y(2)-y(1)];
 
 for i = 1 : length(N)
 
     h = 1/N(i); %Step Length 
-    [t, u] = RK(f, 1, h);
+    [t, u] = RK(f, [1 0], 2, N(i));
     T{i} = t;
     U{i} = u;
+    figure(1);
+    plot(t,u(1,:)');
+    hold on;
+    figure(1);
+    xlabel('time');
+    title('ex 1')
+    hold on
+    
 end
-A = U{1}(1,:);
-t = [t 1];
-plot(t, A);
+legend('10','20','40', '80', '160', '320')
+%Order of accuracy
+en = zeros(1,5);
+temp = U{6}(1,:);
+y1 = temp(end);
+for i = 1 : length(N)-1
+    temp = U{i}(1,:);
+    yn = temp(end);
+    en(i) = y1 - yn;
+end
+
+hLista = ones(1,5);
+for i = 1 : length(N)-1
+   hLista(i) = hLista(i)/N(i); 
+end
+figure(2);
+loglog(hLista,en);
+xlabel('h')
+ylabel('en')
+
+
+%% 
+
+%ex2
+r1 = 0.04;
+r2 = 10^4;
+r3 = 3*10^7;
+f = @(x)[-r1*x(1)+(r2*x(2)*x(3)); (r1*x(1))- (r2*x(2)*x(3))-(r3*(x(2)^2)); r3*(x(2)^2)];
+N = [125, 250, 500, 1000, 2000];
+Steg = [500, 510, 520, 530, 540, 550, 560, 570, 580, 580, 600]; %530 är det minsta N som ger stabilitet
+x0 = [1 0 0];
+
+for i = 1 : length(N)
+   [t, u] = RK(f, x0, 3,N(i));
+  
+    T2{i} = t;
+    U2{i} = u;
+    
+end
+u  = U2{5};
+t = T2{5};
+
+figure(4);
+loglog(t,u(1,:)');
+hold on;
+
+figure(5)
+loglog(t,u(2,:)');
+hold on;
+
+figure(6)
+loglog(t,u(3,:)');
+
+en = U2{5}(:,end)-U2{4}(:,end);
+
+
+%%
+x0 = [1 0 0];
+r1 = 0.04;
+r2 = 10^4;
+r3 = 3*10^7;
+options = odeset('RelTol', 10^-6, 'AbsTol', 10^-8);
+f = @(t, x)[-r1*x(1)+(r2*x(2)*x(3)); (r1*x(1))- (r2*x(2)*x(3))-(r3*(x(2)^2)); r3*(x(2)^2)];
+[t, x] = ode23(f, [0,1], x0', options);
+figure(10)
+plot(t,x(:,2));
+
+%867 utan
+%10-3 : 876
+%10-4 : 878
+%10-5 : 879
+%10-6 : 879
+
+h=0;
+for i = 1 : length(t)-1
+    
+    h(i) = abs(t(i) - t(i+1));
+end
+
+figure(11)
+h = [h 0];
+plot(t, h)
+%tre olika hgrafer????
+
+%%
+
+
+%%
+
+%%
+x0 = [1 0 0];
+r1 = 0.04;
+r2 = 10^4;
+r3 = 3*10^7;
+
+options = odeset('RelTol', 10^-6, 'AbsTol', 10^-8);
+f = @(t, x)[-r1*x(1)+(r2*x(2)*x(3)); (r1*x(1))- (r2*x(2)*x(3))-(r3*(x(2)^2)); r3*(x(2)^2)];
+[t, x] = ode23s(f, [0,1000], x0', options);
--- RK.m
+++ RK.m
@@ -1,18 +1,19 @@
-function [t, u] = RK(f,y0,h)
+function [t, y] = RK(f, y0, inVals,N)
 
  %vector with 320 even steps between 0 and 1
-t = linspace(0,1,100);
-y(1) = 1;
-y(2) = 0;
-y=y';
+y=zeros(inVals,N+1);
+y(:,1)=y0;
+h=1/N;
+t=linspace(0,1,N+1)';
 
-for i = 1:100
-    k1 = f(t(i), y(:,i));
-    k2 = f(t(i)+h, y(:,i)+h*k1);
-    
-    k3= f((t(i))+(h/2), y(:,i)+(h*k1/4)+(h*k2/4));
+
+
+
+for i = 1:N
+    k1 = f(y(:,i));
+    k2 = f(y(:,i)+h*k1);
+    k3= f(y(:,i)+(h*k1/4)+(h*k2/4));
     y(:,i+1) = y(:,i) + ((h/6)*(k1+k2+k3));
-   
     
 end
-u = y;
+
